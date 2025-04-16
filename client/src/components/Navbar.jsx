379876c206ee81e28f02ab2e6c7cba846c22c748
Samyam-Pradhan <pradhansamyam71@gmail.com>
2025-04-16 13:35:25 +0545

    Updated Navbar css

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,33 @@
+import { CiSearch } from "react-icons/ci";
+import "../styles/Navbar.css";
 
-
-const Navbar = ({ onLoginClick, onSignupClick }) => {
+const Navbar = () => {
   return (
     <header>
       <div className="nav-grid">
         <div className="logo">
-          <h1>Digital Bookstore</h1>
+          <h1>Move Mf</h1>
         </div>
         <div className="search-bar">
           <input type="text" placeholder="Search..." />
-          {/* <CiSearch /> */}
+          <CiSearch className="search-icon" />
         </div>
         <nav>
           <ul>
             <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Features</a></li>
-            {/* <li className="auth-item"><a href="#" className="login-btn" onClick={onLoginClick}>Login</a></li>
-            <li className="auth-item"><a href="#" className="signup-btn" onClick={onSignupClick}>SignUp</a></li> */}
+            <li><a href="#">Men</a></li>
+            <li><a href="#">Women</a></li>
+            <li><a href="#">Baby Collection</a></li>
+            <li className="dropdown-container">
+              <a href="#">Pages</a>
+              <div className="nav-dropdown">
+                <a href="#">Login</a>
+                <a href="#">Cart</a>
+                <a href="#">Product Details</a>
+              </div>
+            </li>
+            <li><a href="#">Blog</a></li>     
+            <li><a href="#">Contact</a></li>
           </ul>
         </nav>
       </div>
